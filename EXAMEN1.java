b5538ef05be84a1ef266ad0afeff9579bd4a70d6
cristhellgp <143815782+cristhellgp@users.noreply.github.com>
2023-09-20 08:56:19 -0600

    Add files via upload

---
+++ EXAMEN1.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen1;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EXAMEN1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner lec = new Scanner (System.in);
+        System.out.println("cual es el instrumento comprado ");
+        double instrumento=lec.nextDouble();
+        double ins1=1250.45;
+        double ins2=3743.00;
+        double ins3=2683.78;
+        System.out.println("el nombre del instrumento es ");
+        String nombre=lec.nextLine();      
+        System.out.println("el precio de compra es ");
+        String compra = lec.nextLine();    
+        double intrumento =lec.nextDouble();
+        System.out.println("cual es el instrumento comprado");
+         String nom=lec.nextLine();
+         System.out.println("el nombre del instrumento es ");
+         
+          
+                
+        
+        // TODO code application logic here
+        }
+    
+}
